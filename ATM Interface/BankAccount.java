3cf1e2bbef4bcff039c413e4a73f4e2e93745cc9
Ritika Saini <95163431+RitikaSaini26@users.noreply.github.com>
2023-10-13 23:04:12 +0530

    Create BankAccount.java

---
+++ ATM Interface/BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
